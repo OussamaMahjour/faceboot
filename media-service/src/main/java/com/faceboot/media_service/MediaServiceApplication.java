87eefee4913cf82e104d97a506d1055dd85263cf
Omar Mouqat <omar.dev.23.06.2003@gmail.com>
2025-01-05 15:55:50 +0100

    In test Service

--- media-service/src/main/java/com/faceboot/media_service/MediaServiceApplication.java
+++ media-service/src/main/java/com/faceboot/media_service/MediaServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MediaServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("------------------------------- Hello World ---------------------------------");
 		SpringApplication.run(MediaServiceApplication.class, args);
 
 	}
